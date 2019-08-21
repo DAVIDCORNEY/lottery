81d97cad2c7041f6482c5872be1054c41c84198a
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-21 22:41:06 +0100

    Send LotteryBall number on props from app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import LotteryBall from "../src/components/LotteryBall/LotteryBall";
 function App() {
   return (
     <div className="App">
-      <LotteryBall />
+      <LotteryBall num={10} />
+      <LotteryBall num={17} />
+      <LotteryBall num={21} />
+      <LotteryBall num={36} />
     </div>
   );
 }
