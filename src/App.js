0efdbb6b6519bf7af1475073685c171c2487e059
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-21 22:27:28 +0100

    Import LotteryBall into App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import LotteryBall from "../src/components/LotteryBall/LotteryBall";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <LotteryBall />
+    </div>
+  );
 }
 
 export default App;
