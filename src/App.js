b9ad1d57fc1b844beca680f249c0b0391b449af9
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-22 13:04:06 +0100

    App remove LotteryBall

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import LotteryBall from "../src/components/LotteryBall/LotteryBall";
 import Lottery from "../src/components/Lottery/Lottery";
 
 function App() {
